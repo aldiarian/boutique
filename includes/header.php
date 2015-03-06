c55878c2d635dcb71612aa8c358c1ec06b838179
Alberto Diarian <aldiarian@gmail.com>
2015-03-06 10:56:05 +0100

    favicons

--- includes/header.php
+++ includes/header.php
@@ -27,26 +27,28 @@
 
 <!-- Iconos -->
 
-<!-- 16x16 -->
-<link rel="shortcut icon" href="/favicon.ico">
-<!-- 32x32 -->
-<link rel="shortcut icon" href="/favicon.png">
-<!-- 57x57 (precomposed) for iPhone 3GS, pre-2011 iPod Touch and older Android devices -->
-<link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-precomposed.png">
-<!-- 72x72 (precomposed) for 1st generation iPad, iPad 2 and iPad mini -->
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
-<!-- 114x114 (precomposed) for iPhone 4, 4S, 5 and post-2011 iPod Touch -->
-<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
-<!-- 144x144 (precomposed) for iPad 3rd and 4th generation -->
-<link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
+<link rel="apple-touch-icon" sizes="57x57" href="../images/favicons/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="../images/favicons/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="../images/favicons/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="../images/favicons/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="../images/favicons/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="../images/favicons/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="../images/favicons/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="../images/favicons/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="../images/favicons/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="../images/favicons/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="../images/favicons/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="../images/favicons/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="../images/favicons/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-TileImage" content="../images/favicons//mstile-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
+
 
 <!--iOS -->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<!-- Windows 8 / RT http://bit.ly/HHkt7m -->
-<meta name="msapplication-TileImage" content="img/apple-touch-icon-144x144-precomposed.png">
-<meta name="msapplication-TileColor" content="#000">
-<meta http-equiv="cleartype" content="on">
 
 <!-- jquery-->
 <script src="js/jquery-1.9.1.min.js"></script>
